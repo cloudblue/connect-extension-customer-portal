ebfec5f3f208b7e0142317f3c65d527f8753d01a
rmondal00 <rahul.mondal@ingrammicro.com>
2022-09-12 13:40:04 +0200

    LITE-25104 Custom component - ProductCard

--- client_code/secure/ProductCard/__init__.py
+++ client_code/secure/ProductCard/__init__.py
@@ -1,29 +1,43 @@
 from ._anvil_designer import ProductCardTemplate
-from ..ProductDetailPanel import ProductDetailPanel
+from ...scripts.client import get_connect_base_url
 
 
 class ProductCard(ProductCardTemplate):
-    def __init__(self, page, product, product_list, **properties):
+
+    def cut_short(self, value, size):
+        return (value[0:(size - 3)] + '...') if len(value) > size else value
+
+    def prepare(self, portal_url, product):
+        product['vendor'] = f"by {product['owner']['name']}"
+        if not product['icon'].startswith('http'):
+            product['icon'] = f"{portal_url}{product['icon']}"
+    
+        product['minimize_short_description'] = self.cut_short(
+            product['short_description'],
+            135,
+        )
+    
+        product['minimize_name'] = self.cut_short(product['name'], 25)
+    
+        return product
+  
+    def __init__(self, **properties):
+        self.portal_url = get_connect_base_url()
         self.init_components(**properties)
 
-        self.product = product
-        self.product_list = product_list
-        self.page = page
-
-        self.item = product
+        self.product = properties.get('product')
+    
+    @property
+    def product(self):
+        return self._product
+    
+    @product.setter
+    def product(self, value):
+        self.prepare(self.portal_url, value)
+        self._product = value
 
     def lnk_view_more_click(self, **event_args):
-        if not self.page.product_detail_panel:
-            self.page.product_detail_panel = ProductDetailPanel(
-                self.page,
-                self.product,
-                self.product_list,
-            )
-        else:
-            self.page.product_detail_panel.select_product(self.product)
-        
-        self.page.clear()
-        self.page.add_component(
-            self.page.product_detail_panel,
-            full_width_row=True,
+        self.raise_event(
+          'show_detail',
+          product=self.product,
         )
