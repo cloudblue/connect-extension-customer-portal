8e4a2b34599ac0e990505c4f78ed433697509650
rmondal00 <rahul.mondal@ingrammicro.com>
2022-09-13 11:22:01 +0200

    LITE-25114 Custom component - ProductMenuItem

--- client_code/secure/ProductMenuItem/__init__.py
+++ client_code/secure/ProductMenuItem/__init__.py
@@ -2,13 +2,18 @@ from ._anvil_designer import ProductMenuItemTemplate
 
 
 class ProductMenuItem(ProductMenuItemTemplate):
-    def __init__(self, page, product, **properties):
+    def __init__(self, **properties):
         # Set Form properties and Data Bindings.
-        self.page = page
+        self.item = properties.get('product') or {}
         self.init_components(**properties)
-
-        # Any code you write here will run when the form opens.
-        self.item = product
+    
+    @property
+    def product(self):
+        return self.item
+    
+    @product.setter
+    def product(self, value):
+        self.item = value
 
     def lnk_product_menu_click(self, **event_args):
-        self.page.select_product(self.item)
+        self.raise_event('show_detail', product=self.item)
