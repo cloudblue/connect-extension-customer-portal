af149758134438842afdd3d88c5cd9f0b31191e4
rmondal00 <rahul.mondal@ingrammicro.com>
2022-09-14 14:54:16 +0200

    LITE-25127 Custom components - ProductDetail and SubscriptionShort

---
+++ client_code/secure/ProductDetail/__init__.py
@@ -0,0 +1,21 @@
+from ._anvil_designer import ProductDetailTemplate
+
+class ProductDetail(ProductDetailTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.item = properties.get('product') or {}
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
+  
+  @property
+  def product(self):
+      return self.item
+  
+  @product.setter
+  def product(self, value):
+      self.item = value
+
+  def product_icon_mouse_up(self, x, y, button, **event_args):
+    self.raise_event('show_details', product=self.item)
+
